29107bb0af0195c19a5aa7c7ff7390a17f48e8ee
David Bindel <bindel@cs.cornell.edu>
2019-09-16 09:15:46 -0400

    Killed extraneous "then"s in testnan.m

--- lec/code/testnan.m
+++ lec/code/testnan.m
@@ -1,5 +1,5 @@
 x = 0/0;
-if x < 0 then       disp('x is negative');
-elseif x >= 0 then  disp('x is non-negative');
-else                disp('Uh...');
+if x < 0,      disp('x is negative');
+elseif x >= 0, disp('x is non-negative');
+else           disp('Uh...');
 end
